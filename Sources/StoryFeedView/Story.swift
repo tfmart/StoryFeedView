c75f37a2d3302da09683b990ab8793dd916bd40b
Tomas Martins <tomfmart@gmail.com>
2021-01-02 23:34:45 -0300

    Created public initializer for Story

--- Sources/StoryFeedView/Story.swift
+++ Sources/StoryFeedView/Story.swift
@@ -10,4 +10,9 @@ import UIKit
 public struct Story {
     public var image: UIImage?
     public var headline: String?
+    
+    public init(image: UIImage? = nil, headline: String? = nil) {
+        self.image = image
+        self.headline = headline
+    }
 }
